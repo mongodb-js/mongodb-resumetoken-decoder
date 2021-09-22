82cd950bc373db1ae12ab891c5935156065f2c64
Anna Henningsen <anna@addaleax.net>
2021-09-22 16:38:00 +0200

    fix: use TS syntax that works for Node.js 12
    Refs: https://github.com/mongodb-js/mongodb-resumetoken-decoder/pull/1

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,5 @@
 import { decodeResumeToken } from './';
-if (!process.argv[2]?.match(/([A-Z0-9]{2})*/i)) {
+if (!process.argv[2] || !process.argv[2].match(/([A-Z0-9]{2})*/i)) {
   throw new Error('Usage: mongodb-resumetoken-decoder <hex string>');
 }
 console.dir(decodeResumeToken(process.argv[2]), { depth: Infinity, customInspect: true });
